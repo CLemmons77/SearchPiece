d5f749296738c0c62bc023ebaf04e076d16a76b4
CLemmons77 <caleblego7@gmail.com>
2024-10-15 18:57:26 -0400

    new api

--- src/components/ui/CardResult.jsx
+++ src/components/ui/CardResult.jsx
@@ -3,11 +3,12 @@ import axios from "axios";
 
 const CardResult = () => {
   const [cards, setCards] = useState([]);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-  const fetchCards = async (name) => {
+  const fetchCards = async () => {
     const { data } = await axios.get(
-      `https://apitcg.com/api/one-piece/cards?property=${name}`
+      `curl https://api.cardtrader.com/api/v2/info \
+  -H "Authorization: Bearer eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJjYXJkdHJhZGVyLXByb2R1Y3Rpb24iLCJzdWIiOiJhcHA6MTIzOTkiLCJhdWQiOiJhcHA6MTIzOTkiLCJleHAiOjQ4ODQ3MDYxMzEsImp0aSI6ImEzNDg3YjM3LTAzNGUtNGE3MC1hYWM1LWJhOGY2Yzk0NzEzMSIsImlhdCI6MTcyOTAzMjUzMSwibmFtZSI6IkNhbGVibGVnbzcgQXBwIDIwMjQxMDE1MTg0ODUwIn0.OT1lFZpV2mOVSgWd3TzuhMMSptXBesGcrwnuIMXHTMFJcezprFiQNllbq9U8C23L35SM2nl5NQZ0lFL58V-KydEWPQFhFytSUDyngdMXVRtc-d_GxWzATWThA9OMpI7jDhFuveVeOaYixeE9Qdfrg4fYNFcLfbbkot9Nd_SEiTnYmlQ_tGmGf9N-KsJ1IJnGBKrWjsMCsDU9docHqaqtEH31CXqQPYR8apis3HRLw-NjELC0Xr3f1KY5C5NIyZ5Uce5upPvRrVIeAuzfwwMcU4k5DECS6CQ_2VeTDlNHCX0ExAzLv9kN6Fzsef5pFSsEK7l_uYK2wWtU-EZKWXadTQ`
     );
     setCards(data);
   };
@@ -29,16 +30,20 @@ const CardResult = () => {
           </figure>
           <h3 class="anime__title"></h3>
           <p>
-            <b>Cardset:</b>{card.cardset}
+            <b>Cardset:</b>
+            {card.cardset}
           </p>
           <p>
-            <b>Power:</b>{card.power}
+            <b>Power:</b>
+            {card.power}
           </p>
           <p>
-            <b>Cost:</b>{card.cost}
+            <b>Cost:</b>
+            {card.cost}
           </p>
           <p>
-            <b>Color:</b>{card.color}
+            <b>Color:</b>
+            {card.color}
           </p>
           <p>
             <b>Rarity:</b> <span class="anime__score">{card.rarity}</span>
@@ -47,7 +52,8 @@ const CardResult = () => {
             <b>Family:</b> <span class="anime__score">{card.family}</span>
           </p>
           <p>
-            <b>Ability:</b>{card.ability}
+            <b>Ability:</b>
+            {card.ability}
           </p>
         </div>
       ))}
