09a0326fc5311d5fd99aa77ba07b744dab81bb84
CLemmons77 <caleblego7@gmail.com>
2024-10-18 19:29:52 -0400

    fixed card info page

--- src/pages/CardInfo.jsx
+++ src/pages/CardInfo.jsx
@@ -6,62 +6,62 @@ const CardInfo = () => {
   let navigate = useNavigate();
   const { imageName } = useParams();
 
-  console.log(imageName);
+  const card = cards.find((card) => card.imageName === imageName);
 
   return (
     <section>
-      <div className="header__container" key={cards.imageName}>
+      <div className="header__container" key={card.imageName}>
         <div className="header__text card__container--border">
-          <h1>{cards.cardName}</h1>
-          <h2>{cards.cardSets}</h2>
-          <h2>{cards.cardNumber}</h2>
+          <h1>{card.cardName}</h1>
+          <h2>{card.cardSets}</h2>
+          <h2>{card.cardNumber}</h2>
         </div>
         <div className="header__img--container">
           <figure className="card__img--wrapper">
-            <img src={cards.imageUrl} alt="card image" className="card__img" />
+            <img src={card.imageUrl} alt="card image" className="card__img" />
           </figure>
         </div>
         <div className="card__info--text">
           <h3>
-            <b>Card Type:</b> {cards.cardType}
+            <b>Card Type:</b> {card.cardType}
           </h3>
           <h3>
-            <b>Color:</b> {cards.colors}
+            <b>Color:</b> {card.colors}
           </h3>
           <h3>
-            <b>Rarity:</b> {cards.rarity}
+            <b>Rarity:</b> {card.rarity}
           </h3>
           <h3>
-            <b>Life:</b> {cards.life}
+            <b>Life:</b> {card.life}
           </h3>
           <h3>
-            <b>Attribute:</b> {cards.attributes}
+            <b>Attribute:</b> {card.attributes}
           </h3>
           <h3>
-            <b>Cost:</b> {cards.cost}
+            <b>Cost:</b> {card.cost}
           </h3>
           <h3>
-            <b>Power:</b> {cards.power}
+            <b>Power:</b> {card.power}
           </h3>
           <h3>
-            <b>Counter:</b> {cards.counter}
+            <b>Counter:</b> {card.counter}
           </h3>
           <h3>
-            <b>Type:</b> {cards.types}
+            <b>Type:</b> {card.types}
           </h3>
-          <h3>
-            <b>Ability:</b> {cards.effects}
+          <h3 className="card__info--ability">
+            <b>Ability:</b> {card.effects}
           </h3>
         </div>
       </div>
-        <div className="header__search">
-          <a
-            className="search__btn btn__hover-effect card__back--btn"
-            onClick={() => navigate("/search")}
-          >
-            Back to Search
-          </a>
-        </div>
+      <div className="header__search">
+        <a
+          className="search__btn btn__hover-effect card__back--btn"
+          onClick={() => navigate("/search")}
+        >
+          Back to Search
+        </a>
+      </div>
     </section>
   );
 };
