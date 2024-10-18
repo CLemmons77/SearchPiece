bbea66a6c579dbbdaf65fbdc01e289e4d128ad6b
CLemmons77 <caleblego7@gmail.com>
2024-10-18 14:59:29 -0400

    testing search

--- src/components/ui/CardResult.jsx
+++ src/components/ui/CardResult.jsx
@@ -1,85 +1,44 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Spinner from "../../assets/spinner-solid.svg";
-import { cards } from "../../data";
+import React from "react";
 
-const CardResult = () => {
-  //   const [cards, setCards] = useState([]);
-  const [loading, setLoading] = useState(false);
 
-  const fetchCards = async () => {
-    setLoading(true);
-    // const { data } = await axios.get(
-    //   `https://api.cardtrader.com/api/v2/categories?game_id=15`,
-    //   {
-    //     headers: {
-    //       Authorization:
-    //         "Bearer eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJjYXJkdHJhZGVyLXByb2R1Y3Rpb24iLCJzdWIiOiJhcHA6MTIzOTkiLCJhdWQiOiJhcHA6MTIzOTkiLCJleHAiOjQ4ODQ3MDYxMzEsImp0aSI6ImEzNDg3YjM3LTAzNGUtNGE3MC1hYWM1LWJhOGY2Yzk0NzEzMSIsImlhdCI6MTcyOTAzMjUzMSwibmFtZSI6IkNhbGVibGVnbzcgQXBwIDIwMjQxMDE1MTg0ODUwIn0.OT1lFZpV2mOVSgWd3TzuhMMSptXBesGcrwnuIMXHTMFJcezprFiQNllbq9U8C23L35SM2nl5NQZ0lFL58V-KydEWPQFhFytSUDyngdMXVRtc-d_GxWzATWThA9OMpI7jDhFuveVeOaYixeE9Qdfrg4fYNFcLfbbkot9Nd_SEiTnYmlQ_tGmGf9N-KsJ1IJnGBKrWjsMCsDU9docHqaqtEH31CXqQPYR8apis3HRLw-NjELC0Xr3f1KY5C5NIyZ5Uce5upPvRrVIeAuzfwwMcU4k5DECS6CQ_2VeTDlNHCX0ExAzLv9kN6Fzsef5pFSsEK7l_uYK2wWtU-EZKWXadTQ",
-    //     },
-    //   }
-    // );
-    // setCards(data);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchCards();
-  }, []);
+const CardResult = ({ card }) => {
 
   return (
-    <section id="results">
-      <div className="anime__cards">
-        {loading ? (
-          <div className="result">
-            <figure>
-              <img
-                className="fa-spinner results__loading--spinner"
-                src={Spinner}
-              />
-            </figure>
-          </div>
-        ) : (
-          cards.map((card) => (
-            <div className="anime__card" key={card.id}>
-              <div className="anime__card--container">
-                <figure className="anime__poster--wrapper">
-                  {/* <img
-                    src={}
-                    alt=""
-                    className="anime__poster"
-                  /> */}
-                </figure>
-                <h3 className="anime__title">{card.cardName}</h3>
-                <p>
-                  <b>Cardset:</b> {card.cardSets}
-                </p>
-                <p>
-                  <b>Card Type:</b> {card.cardType}
-                </p>
-                <p>
-                  <b>Color:</b> {card.colors}
-                </p>
-                <p>
-                  <b>Life:</b> {card.life}
-                </p>
-                <p>
-                  <b>Cost:</b> {card.cost}
-                </p>
-                <p>
-                  <b>Power:</b> {card.power}
-                </p>
-                <p>
-                  <b>Type:</b> {card.types}
-                </p>
-                <p>
-                  <b>Ability:</b> {card.effects}
-                </p>
-              </div>
-            </div>
-          ))
-        )}
+    <div className="anime__card" key={card.id}>
+      <div className="anime__card--container">
+        <figure className="anime__poster--wrapper">
+          <img src={card.imageUrl} alt="card image" className="anime__poster" />
+        </figure>
+        <h3 className="anime__title">{card.cardName}</h3>
+        <p>
+          <b>Cardset:</b> {card.cardSets}
+        </p>
+        <p>
+          <b>Card Type:</b> {card.cardType}
+        </p>
+        <p>
+          <b>Color:</b> {card.colors}
+        </p>
+        <p>
+          <b>Rarity:</b> {card.rarity}
+        </p>
+        <p>
+          <b>Life:</b> {card.life}
+        </p>
+        <p>
+          <b>Cost:</b> {card.cost}
+        </p>
+        <p>
+          <b>Power:</b> {card.power}
+        </p>
+        <p>
+          <b>Type:</b> {card.types}
+        </p>
+        <p>
+          <b>Ability:</b> {card.effects}
+        </p>
       </div>
-    </section>
+    </div>
   );
 };
 
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardResult from "../components/ui/CardResult";
+import Spinner from "../assets/spinner-solid.svg";
+import { cards } from "../data";
 
 const Search = () => {
+  const [cardList, setCardList] = useState(cards);
+  const [loading, setLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+
+  const searchCards = () => {
+    filterCards(searchValue);
+  };
+
+
+  const filterCards = (filter) => {
+    if (filter === "ALPHABET_ASCEND") {
+      setCardList(
+        cards.slice().sort((a, b) => a.cardName.localeCompare(b.cardName))
+      );
+    }
+    if (filter === "ALPHABET_DESCEND") {
+      setCardList(
+        cards.slice().sort((a, b) => b.cardName.localeCompare(a.cardName))
+      );
+    }
+    if (filter === "CARD_NUMBER") {
+      setCardList(
+        cards.slice().sort((a, b) => b.cardNumber.localeCompare(a.cardNumber))
+      );
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    filterCards();
+    searchCards()
+  }, []);
+
   return (
     <>
       <section id="search">
@@ -12,20 +48,31 @@ const Search = () => {
             </h1>
             <div className="library__header">
               <div className="library__search">
-                <form action="" onSubmit="onSearchButtonClick(event)">
-                  <input
-                    class="library__search--input"
-                    type="search"
-                    placeholder="Search by name, cardset, or color"
-                    id="search"
-                  />
-                  <button className="library__search--btn btn__hover-effect">
-                    Search
-                  </button>
-                </form>
+                <input
+                  class="library__search--input"
+                  type="search"
+                  placeholder="Search by name, cardset, or color"
+                  id="search"
+                  value={searchValue}
+                  onSubmit={(e) => setSearchValue(e.target.value)}
+                  onKeyPress={(event) => {
+                    event.key === "Enter" && searchCards();
+                  }}
+                />
+                <button
+                  className="library__search--btn btn__hover-effect"
+                  onClick={() => searchCards()}
+                >
+                  Search
+                </button>
               </div>
-              <select id="filter" onChange="filterAnime(event)">
-                <option className="filter__option" value="" disabled selected>
+              <select id="filter" onChange={(e) => filterCards(e.target.value)}>
+                <option
+                  className="filter__option"
+                  value="DEFAULT"
+                  disabled
+                  selected
+                >
                   Sort
                 </option>
                 <option className="filter__option" value="ALPHABET_ASCEND">
@@ -34,13 +81,30 @@ const Search = () => {
                 <option className="filter__option" value="ALPHABET_DESCEND">
                   Alphabetical: Descending
                 </option>
-                <option className="filter__option" value="RATING">
-                  Rarity
+                <option className="filter__option" value="CARD_NUMBER">
+                  Card Number
                 </option>
               </select>
             </div>
           </div>
-          <CardResult />
+          <section id="results">
+            <div className="anime__cards">
+              {loading ? (
+                <div className="result">
+                  <figure>
+                    <img
+                      className="fa-spinner results__loading--spinner"
+                      src={Spinner}
+                    />
+                  </figure>
+                </div>
+              ) : (
+                cardList
+                  .slice(0, 15)
+                  .map((card) => <CardResult card={card} key={card.id} />)
+              )}
+            </div>
+          </section>
         </div>
       </section>
     </>
